63f26904c10a8e4b11009cca733d6fb252ffdef5
Daniel Junior <danielgjjpatrocinio@gmail.com>
2024-07-26 20:28:10 -0300

    lootie loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Lottie from 'react-lottie';
 import './App.css';
 import smallPortrait from './assets/wedding-card-portrait-small.svg';
 import portrait from './assets/wedding-card-portrait.svg';
 import landscape from './assets/wedding-card.svg';
+import loadingAnimation from './assets/loading.json'; 
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -30,9 +32,23 @@ const App = () => {
     };
   }, []);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className="container">
-      {loading && <div className="loading">Loading...</div>}
+      {loading && (
+        <div className="loading">
+          Carregando...
+          <Lottie options={defaultOptions} height={100} width={100} />
+        </div>
+      )}
       <img
         src={imageSrc}
         alt="Wedding Card"
