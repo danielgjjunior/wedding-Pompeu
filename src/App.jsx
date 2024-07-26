d972ffb3cbd97877708322da2bef9ab7492a7f96
Daniel Junior <danielgjjpatrocinio@gmail.com>
2024-07-26 17:51:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeddingCard from './components/WeddingCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WeddingCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeddingCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeddingCard.css';
+import WeddingCardSVG from '../assets/wedding-card.svg'; 
+
+const WeddingCard = () => {
+  return (
+    <div className="card-container">
+    </div>
+  );
+}
+
+export default WeddingCard;
